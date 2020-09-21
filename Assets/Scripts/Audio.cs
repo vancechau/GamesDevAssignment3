44dc7d986cee19f641a9ba5c6f31f02b736b3da4
vancechau <50088207+vancechau@users.noreply.github.com>
2020-09-21 10:40:05 +1000

    Added audio requirements (intro + normal + movement) and fixed ghost animator timing

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [RequireComponent(typeof(AudioSource))]
+ public class Audio: MonoBehaviour
+ {
+     public AudioSource Intro;
+     public AudioSource Normal;
+ 
+    void Start(){
+        Intro.Play();
+        Normal.PlayDelayed(Intro.clip.length);
+    }
+ }
